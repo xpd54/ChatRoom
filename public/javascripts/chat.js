f4b0c1f271c1d49927ac943fbd43ef3a445816a3
Ravi Prakash <raviprakash.xpd54@gmail.com>
2016-05-16 15:37:48 +0530

    Clean up to run the project

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,40 +1,49 @@
 var Chat = function(socket) {
-	this.socket = socket;
+  this.socket = socket;
 };
 
 Chat.prototype.sendMessage = function(room, text) {
-	var message = {
-		room: room,
-		text: text 
-	};
-	this.socket.emit('message', message);
+  var message = {
+    room: room,
+    text: text
+  };
+
+  this.socket.emit('message', message);
 };
 
-
 Chat.prototype.changeRoom = function(room) {
-	this.socket.emit('join', {
-		newRoom: room
-	});
+  this.socket.emit('join', {
+    newRoom: room
+  });
 };
 
 Chat.prototype.processCommand = function(command) {
-	var words = command.split(' ');
-	var command = words[0].substring(1, words[0].length).toLowerCase();
-	var message = false;
-	switch(command) {
-		case 'join':
-		words.shift();
-		var room = words.join(' ');
-		this.changeRoom(room);
-		break;
-		case 'nick':
-		words.shift();
-		var name = words.join(' ');
-		this.socket.emit('nameAttempt', name);
-		break;
-		default:
-		message = 'Unrecognized command.';
-		break;
-	}
-	return message;
+
+  var words = command.split(' ')
+    // Parse command from first word
+    , command = words[0].substring(1, words[0].length).toLowerCase()
+    , message = false;
+
+    switch(command) {
+    // Handle room changing/creation
+    case 'join':
+    words.shift();
+    var room = words.join(' ');
+    this.changeRoom(room);
+    break;
+
+    // Handle name change attempts
+    case 'nick':
+    words.shift();
+    var name = words.join(' ');
+    this.socket.emit('nameAttempt', name);
+    break;
+
+    // Return an error message if the command isn't recognized
+    default:
+    message = 'Unrecognized command.';
+    break;
+  }
+
+  return message;
 };
