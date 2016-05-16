3ac6753c2cf2f55a771400d929479a9c582e9113
Ravi Prakash <raviprakash.xpd54@gmail.com>
2016-05-16 15:27:29 +0530

    Clean Up code

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,10 +5,11 @@ var Chat = function(socket) {
 Chat.prototype.sendMessage = function(room, text) {
 	var message = {
 		room: room,
-		text: text
+		text: text 
 	};
 	this.socket.emit('message', message);
 };
+
 
 Chat.prototype.changeRoom = function(room) {
 	this.socket.emit('join', {
@@ -18,24 +19,22 @@ Chat.prototype.changeRoom = function(room) {
 
 Chat.prototype.processCommand = function(command) {
 	var words = command.split(' ');
-	var command = words[0]
-					.subString(1, words[0].length)
-					.toLowerCase();
-var message = false;
-switch(command) {
-	case 'join': 
+	var command = words[0].substring(1, words[0].length).toLowerCase();
+	var message = false;
+	switch(command) {
+		case 'join':
 		words.shift();
 		var room = words.join(' ');
-		this.changeRoom(room)
+		this.changeRoom(room);
 		break;
-	case 'nick':
+		case 'nick':
 		words.shift();
 		var name = words.join(' ');
-		this.socket.emit('nameAttemt', name);
+		this.socket.emit('nameAttempt', name);
 		break;
-	default:
+		default:
 		message = 'Unrecognized command.';
 		break;
-}
+	}
 	return message;
 };
